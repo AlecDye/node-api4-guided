4b62ec64c8064974e96d2859adbf2dd308f1a2ad
Alec Dye <alecdye@gmail.com>
2020-04-09 09:38:55 -0700

    Adding start script

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+// Goal: Make port dynamic, so that heroku can assign one
+// const port = 5000;
+// use dynamic port OR localhost 5000 (for devs)
+const port = process.env.PORT || 5000;
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
